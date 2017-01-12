8f753dceef3879721c1ce74a5dc68dd1d7ad5a37
orecheng <orecheng@qq.com>
2017-01-12 20:37:59 +0800

    1

--- main.m
+++ main.m
@@ -1,4 +1,10 @@
 clc
 clear all
-circu_air_temp=25;
-circu_air_RH=0.8;
+%% input
+circu_air_temp = 25;
+circu_air_RH = 0.8;
+
+
+
+%% initial
+[circu_air_rho,circu_air_da,circu_air_ha] = rh2da(circu_air_temp,circu_air_RH);
