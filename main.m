d497c9252c9f46faf71488537255debfc7c07755
orecheng <orecheng@qq.com>
2017-01-13 22:27:35 +0800

    添加再生计算

--- main.m
+++ main.m
@@ -21,17 +21,22 @@ reg_trans_air_mass=2;
 
 deh_sink_sol_enth = sol_enthalpy(deh_sink_sol_temp,deh_sink_sol_frac);
 deh_sink_sol_enth_all = deh_sink_sol_enth*deh_sink_sol_mass;
+reg_sink_sol_enth = sol_enthalpy(reg_sink_sol_temp,reg_sink_sol_frac);
+reg_sink_sol_enth_all = reg_sink_sol_enth*reg_sink_sol_mass;
 
 deh_sink_sol_LiCl(1) = deh_sink_sol_frac*deh_sink_sol_mass;
+reg_sink_sol_LiCl(1) = reg_sink_sol_frac*reg_sink_sol_mass;
+
 %% cal
 tic
+step=10;
 timelength=10000;
-step=100;
 for i=2:floor(timelength/step)
 % i=2;
 % while(1)
+%% DEH
     [hp_temp_evap_out,hp_temp_cond_out]...
-        =heatpump(deh_sink_sol_temp(i-1),reg_sink_sol_temp,deh_sink_sol_frac(i-1),reg_sink_sol_frac,deh_trans_sol_mass_in,reg_trans_sol_mass_in);
+        =heatpump(deh_sink_sol_temp(i-1),reg_sink_sol_temp(i-1),deh_sink_sol_frac(i-1),reg_sink_sol_frac(i-1),deh_trans_sol_mass_in,reg_trans_sol_mass_in);
     
     [deh_trans_air_da_out,deh_trans_air_temp_out,deh_trans_sol_temp_out,deh_trans_sol_mass_out,deh_trans_sol_frac_out,deh_trans_sol_enth_out] ...
         = cross_fcn(circu_air_temp,circu_air_RH,hp_temp_evap_out,deh_sink_sol_frac(i-1),deh_trans_air_mass,deh_trans_sol_mass_in);
@@ -53,6 +58,28 @@ for i=2:floor(timelength/step)
     
     deh_sink_sol_temp(i) ...
         =solEnthalpy2Temp(deh_sink_sol_frac(i),deh_sink_sol_enth(i));
+ %% REG  
+    [reg_trans_air_da_out,reg_trans_air_temp_out,reg_trans_sol_temp_out,reg_trans_sol_mass_out,reg_trans_sol_frac_out,reg_trans_sol_enth_out] ...
+        = cross_fcn(circu_air_temp,circu_air_RH,hp_temp_cond_out,reg_sink_sol_frac(i-1),reg_trans_air_mass,reg_trans_sol_mass_in);
+  
+    reg_sink_sol_LiCl(i) ...
+        = reg_sink_sol_LiCl(i-1) + (reg_trans_sol_frac_out*reg_trans_sol_mass_out-reg_sink_sol_frac(i-1)*reg_trans_sol_mass_in)*step;
+    
+    reg_sink_sol_mass(i) ...
+        = reg_sink_sol_mass(i-1) + (reg_trans_sol_mass_out-reg_trans_sol_mass_in)*step;
+
+    reg_sink_sol_frac(i) ...
+        = reg_sink_sol_LiCl(i)/reg_sink_sol_mass(i);
+    
+    reg_sink_sol_enth_all(i) ...
+        = reg_sink_sol_enth_all(i-1)+(reg_trans_sol_enth_out*reg_trans_sol_mass_out-reg_sink_sol_enth(i-1)*reg_trans_sol_mass_in)*step;
+    
+    reg_sink_sol_enth(i) ...
+        = reg_sink_sol_enth_all(i)/reg_sink_sol_mass(i);
+    
+    reg_sink_sol_temp(i) ...
+        =solEnthalpy2Temp(reg_sink_sol_frac(i),reg_sink_sol_enth(i));    
+    
     
 %     frac(i)=deh_sink_sol_frac;
 %     if i>2
